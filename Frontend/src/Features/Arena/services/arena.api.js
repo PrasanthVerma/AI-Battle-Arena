263d9aac7c4a47663767320d9bd86a6bca17c9ee
PrasanthVerma <prasanthkverma4676@gmail.com>
2026-04-28 03:01:58 +0530

    feat: implement axios service modules for authentication and arena API endpoints

--- Frontend/src/Features/Arena/services/arena.api.js
+++ Frontend/src/Features/Arena/services/arena.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URI,
+    baseURL: `${import.meta.env.VITE_API_URI}/api`,
     withCredentials: true,
 })
 
